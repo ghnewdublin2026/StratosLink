1e7a4c99c634f55460627be36fd77a5d5b2a3723
ghnewdublin2026 <github_new_dublin_2026@proton.me>
2026-03-06 01:19:05 +0000

    Update StratosLink_ESP32.ino

--- StratosLink_ESP32.ino
+++ StratosLink_ESP32.ino
@@ -1,38 +1,48 @@
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
-#include <SparkFun_u-blox_GNSS_Arduino_Library.h> 
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
+#include <Wire.h>
 
 SFE_UBLOX_GNSS myGNSS;
-BLECharacteristic *pCharacteristic;
+BLECharacteristic *pChar;
+bool deviceConnected = false;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pS) { deviceConnected = true; };
+    void onDisconnect(BLEServer* pS) { deviceConnected = false; pS->getAdvertising()->start(); }
+};
 
 void setup() {
-  Serial.begin(115200);
   Wire.begin();
-  
-  if (myGNSS.begin() == false) {
-    Serial.println("u-blox GNSS not detected.");
-    while (1);
-  }
+  if (!myGNSS.begin()) while (1); // Halt if no GNSS
 
-  BLEDevice::init("Precision-GPS-Rover");
+  BLEDevice::init("StratosLink-Rover");
   BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
   BLEService *pService = pServer->createService("181a");
-  pCharacteristic = pService->createCharacteristic("2a67", BLECharacteristic::PROPERTY_NOTIFY);
+  pChar = pService->createCharacteristic("2a67", BLECharacteristic::PROPERTY_NOTIFY);
   pService->start();
   pServer->getAdvertising()->start();
 }
 
 void loop() {
-  // Get high-precision data from u-blox
-  float lat = myGNSS.getLatitude() / 10000000.0;
-  float lon = myGNSS.getLongitude() / 10000000.0;
-  float acc = myGNSS.getHorizontalAccuracy() / 1000.0; // Convert mm to meters
+  if (myGNSS.getPVT() && deviceConnected) {
+    float lat = myGNSS.getLatitude() / 10000000.0;
+    float lon = myGNSS.getLongitude() / 10000000.0;
+    float acc = myGNSS.getHorizontalAccuracy() / 1000.0;
+    uint8_t siv = (uint8_t)myGNSS.getSIV();
+    uint8_t fix = (uint8_t)myGNSS.getFixType();
 
-  float data[3] = {lat, lon, acc};
-  pCharacteristic->setValue((uint8_t*)data, 12);
-  pCharacteristic->notify();
-  
-  delay(100); // 10Hz Update rate
+    uint8_t pkg[14];
+    memcpy(&pkg[0], &lat, 4);
+    memcpy(&pkg[4], &lon, 4);
+    memcpy(&pkg[8], &acc, 4);
+    pkg[12] = siv;
+    pkg[13] = fix;
+
+    pChar->setValue(pkg, 14);
+    pChar->notify();
+  }
+  delay(200);
 }
-
